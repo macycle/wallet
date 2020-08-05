3d6e41c7ceac4f4508df5d283c4e521dd7203de2
macycle <1124942097@qq.com>
2020-08-05 13:44:55 +0800

    创建了icon组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="'#'+name" />
+  </svg>
+</template>
+
+<script lang="ts">
+// eslint-disable-next-line prefer-const
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => {
+  return requireContext.keys().forEach(requireContext);
+};
+try {
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+export default {
+  props: ["name"],
+   name: "Icon"
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
