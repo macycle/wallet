a9db6a75dc1b93b67e26c489101262f7f691c9a8
macycle <1124942097@qq.com>
2020-08-05 14:04:53 +0800

    添加内容样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,38 @@
 <template>
-    <div class="nav">
-        <router-link to="/labels">
+    <nav class="nav">
+        <router-link to="/labels" class="item">
             <Icon name='labels'/>
             标签
         </router-link>
-        <router-link to="/money">
+        <router-link to="/money" class="item">
             <Icon name='money'/>
         记账
         </router-link>
-        <router-link to="/statistics">
+        <router-link to="/statistics" class="item">
             <Icon name='statistics'/>
         统计
         </router-link>
-    </div>
+    </nav>
 </template>
 
 
 <style lang="scss" scoped>
     .nav{
-        border:1px solid red;
+        display: flex;
+        box-shadow:0 0 3px rgba(0,0,0,0.25);
+        flex-direction: row;
+        font-size: 12px;
+        > .item{
+            padding: 2px 0;
+            display: flex;
+            flex:1;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            .icon{
+                width: 32px;
+                height: 32px;
+            }
+        }
     }
 </style>
