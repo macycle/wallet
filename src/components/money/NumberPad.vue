429972fe369b04844f3d809396bbc375e11f5a04
macycle <1124942097@qq.com>
2020-08-06 13:01:59 +0800

    完成note

--- src/components/money/NumberPad.vue
+++ src/components/money/NumberPad.vue
@@ -1,28 +1,61 @@
 <template>
     <div class="numberPad">
-            <div class="output">100</div>
+            <div class="output">{{output}}</div>
             <div class="buttons">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>删除</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>清空</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
+                <button @click="inputContent">1</button>
+                <button @click="inputContent">2</button>
+                <button @click="inputContent">3</button>
+                <button @click="remove">删除</button>
+                <button @click="inputContent">4</button>
+                <button @click="inputContent">5</button>
+                <button @click="inputContent">6</button>
+                <button @click="clear">清空</button>
+                <button @click="inputContent">7</button>
+                <button @click="inputContent">8</button>
+                <button @click="inputContent">9</button>
                 <button class="ok">ok</button>
-                <button class="zero">0</button>
-                <button>.</button>
+                <button class="zero" @click="inputContent">0</button>
+                <button @click="inputContent">.</button>
             </div>
     </div>
 </template>
 
-<script>
-    export default {
-        name:'numberPad'
+<script lang='ts'>
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+    export default class NumberPad extends Vue{
+        output='0';
+        inputContent(event: MouseEvent){
+            const button=(event.target as HTMLButtonElement);
+            const input =button.textContent as string;
+            if(this.output.length>=16){return;}   //长度只能是16位
+            if(this.output ==='0'){
+                if('0123456789'.indexOf(input)>=0){
+                    this.output=input;
+                }else{
+                    this.output+=input;
+                }
+                return;
+            }
+            if(this.output.indexOf('.')>=0){
+                if(input==='.'){return ;}
+            }
+            this.output+=input;
+        }
+        remove(){
+            if(this.output.length===1){    //点击它的时候才会判断，如果outpu里面只剩下一位，就替换；
+                this.output='0';
+            }else{
+                this.output=this.output.slice(0,-1)
+            }
+        }
+
+        clear(){
+            this.output='0'
+        }
+
     }
 </script>
 
@@ -33,6 +66,7 @@
         font-family: Consolas,monospace;
         padding: 9px 16px;
         text-align: right;
+        height:72px;
         box-shadow:inset 0 -5px 5px -5px fade_out(black,0.6),
                    inset 0 5px 5px -5px fade_out(black,0.6);
 
